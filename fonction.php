7f33d78da210ade0268011482a5e5a8b685d3c9b
Guewaud <103487430+Guewaud@users.noreply.github.com>
2023-03-19 18:28:56 +0100

    connexion
    j'ai mis la fonction du hash en commentaire en attendant

--- fonction.php
+++ fonction.php
@@ -1,4 +1,36 @@
 <?php
+
+
+function connexion()
+{
+  try {
+    $pdo = new PDO('mysql:dbname=ecebook;host=127.0.0.1', 'root', '');
+    $pdo->exec("SET CHARACTER SET utf8mb4");
+  } catch (PDOException $e) {
+    $pdo = new PDO('mysql:dbname=ecebook;host=127.0.0.1;port=3307', 'root', '');
+    $pdo->exec("SET CHARACTER SET utf8mb4");
+  }
+  return $pdo;
+}
+
+// function encode($mdp, $mail)
+// {
+//   $salt = "@|-°+==00001ddQ";
+//   $crypt = md5($mdp . $salt . $mail);
+//   return $crypt;
+// }
+
+function connecte()
+{
+  if (!isset($_SESSION["idu"])) {
+    return False;
+  } else return True;
+}
+
+
+
+
+
 function footer()
 {
 ?>
