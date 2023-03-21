f6cf0e30fa2da183ca15acc1efa868e0cb1fed2e
Guewaud <103487430+Guewaud@users.noreply.github.com>
2023-03-21 15:34:30 +0100

    modifuser

--- modifuser.php
+++ modifuser.php
@@ -13,8 +13,8 @@ $user = $stmt->fetch();
 
 if (isset($_POST["bouton"])) {
   extract($_POST);
-  $sql = "UPDATE user SET pnom=?, nom=?, mdp=?, naissance=?, promo=?, ville=?, descrip=?, interet=? WHERE idu=?";
-  $pdo->prepare($sql)->execute([$pnom, $nom, $mdp, $naissance, $promo, $ville, $descrip, $interet, $idu]);
+  $sql = "UPDATE user SET nom=?, pnom=?, mdp=?, naissance=?, promo=?, ville=?, descrip=?, interet=? WHERE idu=?";
+  $pdo->prepare($sql)->execute([$nom, $pnom, $mdp, $naissance, $promo, $ville, $descrip, $interet, $idu]);
   header("location:index.php");
 }
 ?>
@@ -36,21 +36,21 @@ if (isset($_POST["bouton"])) {
 <br><br><br>
     <div class="container">
         <div class="row">
-            <div class="col-2">
+            <div class="col-xl-2 col-lg-2 col-md-1 col-sm-1">
             </div>
 
-            <div class="col-8">
-                <div id="mid">
+            <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
+                <div id="mid"><br>
                 <h3>Modifier Profil</h3><br>
 
                 <div>  
                     <div class="container">
                         <div class="row">
                             <div class="col-4" id="legende">
-                                <label id="pn" for="validationDefault01" class="form-label">Prénom</label><span class="etoile">*</span>
+                                <label id="pn" for="validationDefault01" class="form-label">Nom</label><span class="etoile">*</span>
                             </div>
                             <div class="col-6">
-                                <input type="text" name="pnom" class="form-control" id="second" value="<?= $user["pnom"] ?>" required>
+                                <input type="text" name="pnom" class="form-control" id="second" value="<?= $user["nom"] ?>" required>
                             </div>
                             <div class="col-2">
                             </div>
@@ -62,10 +62,10 @@ if (isset($_POST["bouton"])) {
                     <div class="container">
                         <div class="row">
                             <div class="col-4" id="legende">
-                                <label id="pn" for="validationDefault02" class="form-label">Nom</label><span class="etoile">*</span>
+                                <label id="pn" for="validationDefault02" class="form-label">Prénom</label><span class="etoile">*</span>
                             </div>
                             <div class="col-6">
-                            <input type="text" name="nom" class="form-control" id="second" value="<?= $user["nom"] ?>" required>
+                            <input type="text" name="nom" class="form-control" id="second" value="<?= $user["pnom"] ?>" required>
                             </div>
                             <div class="col-2">
                             </div>
@@ -77,7 +77,7 @@ if (isset($_POST["bouton"])) {
                     <div class="container">
                         <div class="row">
                             <div class="col-4" id="legende">
-                                <label for="validationDefault02" class="form-label">Mot de Passe</label><span class="etoile">*</span>
+                                <label id="pn" for="validationDefault02" class="form-label">Mot de Passe</label><span class="etoile">*</span>
                             </div>
                             <div class="col-6">
                                 <input type="text" name="mdp" class="form-control" id="second" value="<?= $user["mdp"] ?>" required>
@@ -92,7 +92,7 @@ if (isset($_POST["bouton"])) {
                     <div class="container">
                         <div class="row">
                             <div class="col-4" id="legende">
-                                <label for="validationDefault02" class="form-label">Date de naissance</label><span class="etoile">*</span>
+                                <label id="pn" for="validationDefault02" class="form-label">Date de naissance</label><span class="etoile">*</span>
                             </div>
                             <div class="col-6">
                                 <input type="text" name="naissance" class="form-control" id="second" value="<?= $user["naissance"] ?>"required>
@@ -107,7 +107,7 @@ if (isset($_POST["bouton"])) {
                     <div class="container">
                         <div class="row">
                             <div class="col-4" id="legende">
-                                <label for="validationDefaultUsername" class="form-label">Adresse mail</label>
+                                <label id="pn" for="validationDefaultUsername" class="form-label">Adresse mail</label>
                             </div>
                             <div class="col-6">
                                 <div class="input-group">
@@ -125,7 +125,7 @@ if (isset($_POST["bouton"])) {
                     <div class="container">
                         <div class="row">
                             <div class="col-4" id="legende">
-                                <label for="validationDefault04" class="form-label">Promo</label><span class="etoile">*</span>
+                                <label id="pn" for="validationDefault04" class="form-label">Promo</label><span class="etoile">*</span>
                             </div>
                             <div class="col-6">
                                 <input type="text" name="promo" class="form-control" id="second" value="<?= $user["promo"] ?>" required>
@@ -140,7 +140,7 @@ if (isset($_POST["bouton"])) {
                     <div class="container">
                         <div class="row">
                             <div class="col-4" id="legende">
-                                <label for="6" class="form-label">Ville</label><span class="etoile">*</span>
+                                <label id="pn" for="6" class="form-label">Ville</label><span class="etoile">*</span>
                             </div>
                             <div class="col-6">
                                 <input type="text" name="ville" class="form-control" id="second" value="<?= $user["ville"] ?>" required>
@@ -155,7 +155,7 @@ if (isset($_POST["bouton"])) {
                     <div class="container">
                         <div class="row">
                             <div class="col-4" id="legende">
-                                <label for="validationDefault04" class="form-label">Description</label><span class="etoile">*</span>
+                                <label id="pn" for="validationDefault04" class="form-label">Description</label><span class="etoile">*</span>
                             </div>
                             <div class="col-6">
                                 <input type="text" name="descrip" class="form-control" id="second" value="<?= $user["descrip"] ?>" required>
@@ -170,7 +170,7 @@ if (isset($_POST["bouton"])) {
                     <div class="container">
                         <div class="row">
                             <div class="col-4" id="legende">
-                                <label for="validationDefault05" class="form-label">Interet</label><span class="etoile">*</span>
+                                <label id="pn" for="validationDefault05" class="form-label">Interet</label><span class="etoile">*</span>
                             </div>
                             <div class="col-6">
                                 <input type="text" name="interet" class="form-control" id="second" value="<?= $user["interet"] ?>" required>
@@ -181,35 +181,6 @@ if (isset($_POST["bouton"])) {
                     </div>
                 </div><br>
 
-                <div>  
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-4" id="legende">
-                                <label for="validationDefault02" class="form-label">Date de naissance</label><span class="etoile">*</span>
-                            </div>
-                            <div class="col-6">
-                                <input type="text" name="naissance" class="form-control" id="second" value="<?= $user["naissance"] ?>"required>
-                            </div>
-                            <div class="col-2">
-                            </div>
-                        </div>
-                    </div>
-                </div><br>
-
-                <div>  
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-4" id="legende">
-                                <label for="validationDefault02" class="form-label">Date de naissance</label><span class="etoile">*</span>
-                            </div>
-                            <div class="col-6">
-                                <input type="text" name="naissance" class="form-control" id="second" value="<?= $user["naissance"] ?>"required>
-                            </div>
-                            <div class="col-2">
-                            </div>
-                        </div>
-                    </div>
-                </div><br><br>
 
                 <div>  
                     <div class="container">
@@ -217,7 +188,7 @@ if (isset($_POST["bouton"])) {
                             <div class="col-3">
                             </div>
                             <div class="col-6">
-                                <button type="button" class="btn btn-warning" name="bouton2" style="margin-left:15%"><a href="modifmdp.php" style="text-decoration:none" name="bouton2">Modifier mon mot de passe</a></button><br><br>
+                                <button type="button" class="btn btn-warning" name="bouton2" style="margin-left:15%"><a href="modifmdp.php" style="text-decoration:none">Modifier mon mot de passe</a></button><br><br>
                             </div>
                             <div class="col-3">
                             </div>
@@ -225,13 +196,13 @@ if (isset($_POST["bouton"])) {
                     </div>
                 </div><br>
 
-                    <div class="d-grid gap-2 col-6 mx-auto">
-                        <input class="btn btn-success text-center" type="submit" value="Modifier" name="bouton"><br><br>
-                    </div>
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <input class="btn btn-success text-center" type="submit" value="Modifier" name="bouton"><br><br>
+                </div>
 
                 </div>
             </div>
-            <div class="col-2">
+            <div class="col-xl-2 col-lg-2 col-md-1 col-sm-1">
             </div>
 
         </div>
